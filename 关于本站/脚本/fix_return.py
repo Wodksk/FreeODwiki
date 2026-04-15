fb0e0d6020f6ac9d0aec13031905f54e189e1e6b
BarbTurnip437 <zta-4ucsk8dla@dingtalk.com>
2026-04-16 00:05:06 +0800

    继续尝试修复... #61

--- 关于本站/脚本/fix_return.py
+++ 关于本站/脚本/fix_return.py
@@ -2,20 +2,31 @@ import re
 from pathlib import Path
 from difflib import unified_diff
 
-PATTERN = re.compile(r"\n*(\[◀返回\].+?\))\n+(# .+?)\n")
-REPLACEMENT = r"\2\n\n\1\n"
+PATTERN_HASHTAG = re.compile(
+    r"\n*^(\[.?返回\].+?\))\n+# (.+?)\n",
+    re.MULTILINE,
+)
+PATTERN_UNDERLINE = re.compile(
+    r"\n*^(\[.?返回\].+?\))\n+?(.+?)\n=+\n",
+    re.MULTILINE,
+)
+REPLACEMENT = r"# \2\n\n\1\n"
 
 for file in Path(".").rglob("*.md"):
     content = file.read_text("utf-8")
-    fixed_content = PATTERN.sub(REPLACEMENT, content)
+
+    fixed_content = content
+    for pattern in {PATTERN_HASHTAG, PATTERN_UNDERLINE}:
+        if pattern.match(content):
+            fixed_content = pattern.sub(REPLACEMENT, content)
 
     diff = [
         *unified_diff(
             content.splitlines(),
             fixed_content.splitlines(),
-            n=1,
+            n=2,
         )
     ]
     if diff:
-        if not input("\n".join(diff)).lower().startswith(("n", "f")):
+        if not input("\n".join(diff) + "\n>").lower().startswith(("n", "f")):
             file.write_text(fixed_content, "utf-8")
